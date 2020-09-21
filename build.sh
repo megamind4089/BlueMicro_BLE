0e7e16cea958146ef8b034510fa30d0d82486856
Mega Mind <68985133+megamind4089@users.noreply.github.com>
2020-09-21 23:39:29 +0800

    Added keymap for ivan

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ rm -rf output
 
 cd build
 ./build-archlinux  -v lily58:default:left:nrfmicro13 -nrf52840
-./build-archlinux  -v lily58:default:right:nrfmicro13 -nrf52840
+./build-archlinux  -v lily58:default:right:nice_nano -nrf52840
 
 cd ..
 rm -rf ~/downloads/lily_blue_*
 ./uf2conv.py output/lily58/lily58-pca10056-nrfmicro13-default-left.hex -c -f 0xADA52840 -o ~/downloads/lily_blue_left.uf2
-./uf2conv.py output/lily58/lily58-pca10056-nrfmicro13-default-right.hex -c -f 0xADA52840 -o ~/downloads/lily_blue_right.uf2
+./uf2conv.py output/lily58/lily58-pca10056-nice_nano-default-right.hex -c -f 0xADA52840 -o ~/downloads/lily_blue_right.uf2
 
 
